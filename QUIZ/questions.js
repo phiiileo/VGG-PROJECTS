7645a7561a9287b2238405e4b94a856d0614a7f5
Phiiileo <emmanuelboluwajib1@gmail.com>
2020-02-23 22:02:55 +0100

    updated the question

--- QUIZ/questions.js
+++ QUIZ/questions.js
@@ -1,113 +1,113 @@
 const MyQuestions = [
     {
         id: 1,
-        question: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus, nostrum architectomaiores laborum eos modifhfhhfhhfhfh.",
+        question: "Which of the following method can you to remove setInterval()?",
         options: [
-            { option: "A", text: "love" },
-            { option: "B", text: "craxy" },
-            { option: "C", text: "mad o" },
-            { option: "D", text: "go ruby" }
+            { option: "A", text: "<code>removeInterval( )</code>" },
+            { option: "B", text: "<code>clearInterval( )</code>" },
+            { option: "C", text: "<code>deleteInterval( )</code>" },
+            { option: "D", text: "<code>removeInterval(false)</code>" }
+        ],
+        answer: "B"
+    },
+    {
+        id: 2,
+        question: "Which of the follow date return the current time in milliseconds?",
+        options: [
+            { option: "A", text: "<code>Date.now( )</code>" },
+            { option: "B", text: "<code>new Date()</code>" },
+            { option: "C", text: "<code>getDate( )</code>" },
+            { option: "D", text: "<code>getMilliseconds( )</code>" }
         ],
         answer: "A"
     },
     {
-        id: 2,
-        question: "Lorem ipsum dolor,fhfhfhfhbfgbhfhfg sit amet consectetur adipisicing elit. Delectus, nostrum architectomaiores laborum eos modi.",
-        options: [
-            { option: "A", text: "Hello from other side" },
-            { option: "B", text: "Jake" },
-            { option: "C", text: "Frank" },
-            { option: "D", text: "Belo" }
-        ],
-        answer: "D"
-    },
-    {
         id: 3,
-        question: "ghghghgh.",
+        question: "Javascript code can be executed on the console?",
         options: [
-            { option: "A", text: " other side" },
-            { option: "B", text: "Jake" },
-            { option: "C", text: "Frank" },
-            { option: "D", text: "Belo" }
+            { option: "A", text: "Depends on browser" },
+            { option: "B", text: "Yes" },
+            { option: "C", text: "I think so" },
+            { option: "D", text: "No" }
         ],
-        answer: "D"
+        answer: "B"
     },
     {
         id: 4,
-        question: "fgfgfgfgf.",
+        question: "Which is the correct way to set background image?",
         options: [
-            { option: "A", text: " other fhfhfh" },
-            { option: "B", text: "Jakfffue" },
-            { option: "C", text: "Frfififank" },
-            { option: "D", text: "Bffielo" }
+            { option: "A", text: "background_image: url(path}" },
+            { option: "B", text: "background-image: (url-link)" },
+            { option: "C", text: "background_image: (url-link)" },
+            { option: "D", text: "background-image: url(path)" }
         ],
         answer: "D"
     },
     {
         id: 5,
-        question: "fgfgfgfgf.",
+        question: "what does prompt() do?",
         options: [
-            { option: "A", text: " other fhfhfh" },
-            { option: "B", text: "Jakfffue" },
-            { option: "C", text: "Frfififank" },
-            { option: "D", text: "Bffielo" }
+            { option: "A", text: "Asks and take input from user" },
+            { option: "B", text: "Alert the user " },
+            { option: "C", text: "Print out information on the console" },
+            { option: "D", text: "it works similar to console.log()" }
         ],
-        answer: "D"
+        answer: "A"
     },
     {
         id: 6,
-        question: "fgfgfgfgf.",
+        question: "Which of the following has the highest precedence?",
         options: [
-            { option: "A", text: " other fhfhfh" },
-            { option: "B", text: "Jakfffue" },
-            { option: "C", text: "Frfififank" },
-            { option: "D", text: "Bffielo" }
+            { option: "A", text: " .wrapper" },
+            { option: "B", text: "#wrapper" },
+            { option: "C", text: ">.wrapper" },
+            { option: "D", text: "*" }
         ],
-        answer: "D"
+        answer: "B"
     },
     {
         id: 7,
-        question: "fgfgfgfgf.",
+        question: "what is bootstrap latest version?",
         options: [
-            { option: "A", text: " other fhfhfh" },
-            { option: "B", text: "Jakfffue" },
-            { option: "C", text: "Frfififank" },
-            { option: "D", text: "Bffielo" }
+            { option: "A", text: "BS" },
+            { option: "B", text: "BS 3" },
+            { option: "C", text: "BS 4" },
+            { option: "D", text: "BS 3.4" }
         ],
-        answer: "D"
+        answer: "C"
     },
     {
         id: 8,
-        question: "fgfgfgfgf.",
+        question: "Which html element is used to insert icons on web page",
         options: [
-            { option: "A", text: " other fhfhfh" },
-            { option: "B", text: "Jakfffue" },
-            { option: "C", text: "Frfififank" },
-            { option: "D", text: "Bffielo" }
+            { option: "A", text: "i" },
+            { option: "B", text: "insert" },
+            { option: "C", text: "icon" },
+            { option: "D", text: "glyph" }
         ],
-        answer: "D"
+        answer: "A"
     },
     {
         id: 9,
-        question: "fgfgfgfgf.",
+        question: "Which of the follow is JS framework?",
         options: [
-            { option: "A", text: " other fhfhfh" },
-            { option: "B", text: "Jakfffue" },
-            { option: "C", text: "Frfififank" },
-            { option: "D", text: "Bffielo" }
+            { option: "A", text: " React" },
+            { option: "B", text: "Ionic" },
+            { option: "C", text: "Angular" },
+            { option: "D", text: "Jquery" }
         ],
-        answer: "D"
+        answer: "C"
     },
     {
         id: 10,
-        question: "fgfgfgfgf.",
+        question: "Veu is a _________",
         options: [
-            { option: "A", text: " other fhfhfh" },
-            { option: "B", text: "Jakfffue" },
-            { option: "C", text: "Frfififank" },
-            { option: "D", text: "Bffielo" }
+            { option: "A", text: "JS library" },
+            { option: "B", text: "JS framework" },
+            { option: "C", text: "JS pluggin" },
+            { option: "D", text: "JS SDK" }
         ],
-        answer: "D"
+        answer: "B"
     }
 
 ]
